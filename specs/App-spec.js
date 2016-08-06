c81e807569075128ef7437541e960f6999436f2f
Christian <christian@ramirezdev.com>
2016-08-06 16:10:44 -0700

    rename spec

--- specs/App-spec.js
+++ specs/App-spec.js
@@ -1,13 +1,12 @@
-var App = require('./../app/App.js');
+var App = require('./../app/App.jsx');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 
 describe("App", function() {
 
-  it("should render text: Hello world!", function() {
+  it("should render demo", function() {
     var app = TestUtils.renderIntoDocument(React.createElement(App));
-    expect(ReactDOM.findDOMNode(app).textContent).toEqual('Hello world!');
   });
 
 });
